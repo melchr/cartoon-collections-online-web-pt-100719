4487ad126f1c5deb59b7582e0d5eb8e46b9fee72
Melissa Christman <melchrstmn@gmail.com>
2019-10-16 17:31:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls(array)
-  array.length > 4
+def long_planeteer_calls(calls)
+  longer_than_four = 
+  calls.length > 4
 end
 
 def find_the_cheese# code an argument here
